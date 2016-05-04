3ce4514abf768cc822619b92b13f97ad4f786bea
Abdulelah Almazyad <aalmazyad@mail.csuchico.edu>
2016-05-04 16:31:24 -0700

    done almost

--- homeFoods/db/schema.rb
+++ homeFoods/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160503000330) do
+ActiveRecord::Schema.define(version: 20160504220557) do
 
   create_table "average_caches", force: :cascade do |t|
     t.integer  "rater_id"
